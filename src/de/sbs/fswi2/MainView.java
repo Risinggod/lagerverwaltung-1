e010d9b85e11d7d0ecc1e455829584b17e379c15
philippho01 <hofmann2001.ph@gmail.com>
2023-11-06 10:46:08 +0100

    new changes

--- src/de/sbs/fswi2/MainView.java
+++ src/de/sbs/fswi2/MainView.java
@@ -1,14 +1,25 @@
 package de.sbs.fswi2;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainView extends Application {
-    public static void main(String[] args) throws Exception {
-        launch(args);
-    }
+
 
     @Override
-    public void start(Stage stage) throws Exception {
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("./mainview.fxml"));
+		loader.setRoot(new BorderPane());
+		Parent rootNode = loader.load();
+		Scene scene = new Scene(rootNode);
+		primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
     }
 }
---
+++ src/de/sbs/fswi2/MainViewController.java
@@ -0,0 +1,28 @@
+package de.sbs.fswi2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+
+public class MainViewController implements Initializable{
+
+    @FXML
+    private void beenden(ActionEvent event) {
+        Platform.exit();
+    }
+    @FXML
+    private Label lblDatum;
+
+
+     @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        lblDatum.setText("Hallo");
+    }
+    
+}
